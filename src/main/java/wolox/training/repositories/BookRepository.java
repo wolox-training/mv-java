b9982022780ef576ec20d3b5f110879a979227fb
Matias Villalba <matias.villalba@wolox.com.ar>
2021-04-09 11:11:43 -0300

    book repository created

---
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -0,0 +1,12 @@
+package wolox.training.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import wolox.training.models.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    public Book findByAuthor(String author);
+
+}

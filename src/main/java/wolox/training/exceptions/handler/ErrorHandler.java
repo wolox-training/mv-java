498aa9bb69e407448128c11e4bfb96e596305bac
Matias Villalba <matias.villalba@wolox.com.ar>
2021-04-12 11:06:23 -0300

    book crud operation and exceptions created

---
+++ src/main/java/wolox/training/exceptions/handler/ErrorHandler.java
@@ -0,0 +1,33 @@
+package wolox.training.exceptions.handler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import wolox.training.exceptions.BookIdMismatchException;
+import wolox.training.exceptions.BookNotFoundException;
+
+@ControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(BookNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ResponseBody
+    protected ErrorResponse BookNotFoundException(BookNotFoundException ex) {
+
+        return new ErrorResponse(ErrorsEnum.JSON_BOOK_NOT_FOUND_EXCEPTION.getCode(),
+                ErrorsEnum.JSON_BOOK_NOT_FOUND_EXCEPTION.getMessage());
+    }
+
+    @ExceptionHandler(BookIdMismatchException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ResponseBody
+    protected ErrorResponse BookIdMismatchException(BookIdMismatchException ex) {
+
+        return new ErrorResponse(ErrorsEnum.JSON_BOOK_ID_MISMATCH_EXCEPTION.getCode(),
+                ErrorsEnum.JSON_BOOK_ID_MISMATCH_EXCEPTION.getMessage());
+    }
+
+}
--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,13 +1,15 @@
 package wolox.training.repositories;
 
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import wolox.training.models.Book;
-import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     Optional<Book> findByAuthor(String author);
 
+    List<Book> findByTitle(String bookTitle);
 }

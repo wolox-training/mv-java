0f311e63b53a45bdd591751a82dc10dd8554516c
Matias Villalba <matias.villalba@wolox.com.ar>
2021-04-14 11:15:25 -0300

    I delete unused imports

--- src/main/java/wolox/training/models/Users.java
+++ src/main/java/wolox/training/models/Users.java
@@ -1,17 +1,13 @@
 package wolox.training.models;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import wolox.training.exceptions.BookAlreadyOwnedException;
 import wolox.training.exceptions.BookNotFoundException;

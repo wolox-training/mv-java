514bb851f1c025e93eeb1a8d94384c05131582b7
Matias Villalba <matias.villalba@wolox.com.ar>
2021-04-14 10:55:29 -0300

    I add returning Optional in findByName method in UserRepository class

--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package wolox.training.repositories;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import wolox.training.models.Users;
@@ -7,6 +8,6 @@ import wolox.training.models.Users;
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
 
-    Users findByName(String name);
+    Optional<Users> findByName(String name);
 
 }

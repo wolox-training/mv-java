0d64fc02c2235f256ce858f7f6a35f5c8dd1bd76
Matias Villalba <matias.villalba@wolox.com.ar>
2021-04-22 11:14:54 -0300

    I add method that returns the name of the logged in user

--- src/main/java/wolox/training/controllers/UserController.java
+++ src/main/java/wolox/training/controllers/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import wolox.training.exceptions.UserIdMismatchException;
@@ -127,5 +130,10 @@ public class UserController {
                 .orElseThrow(UserNotFoundException::new);
         user.deleteBook(book.getBookId());
         return userRepository.save(user);
+    }
+
+    @GetMapping("/username")
+    public String currentUserName(Authentication authentication) {
+        return authentication.getName();
     }
 }
